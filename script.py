7d57d3b90ae9bbdd20adee65d24daff9eaa8865c
PomeloJoestar <noe.nizou@gmail.com>
2023-04-22 02:16:22 +0200

    update

--- script.py
+++ script.py
@@ -53,6 +53,83 @@ map.save("carte.html")
 map_free.save("carte_free.html")
 map_charged.save("carte_charged.html")
 
-with open('amounts.txt', 'w') as f:
-    f.write(str(len(stations_gratuites) + len(stations_payantes)) + "," + str(len(stations_gratuites)) + "," + str(len(stations_payantes)))
+with open("index.html", "w", encoding="utf-8") as f:
+    f.write("""
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Projet NSI</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="script" href="script.js"/>
+</head>
+<body>
+    <h1 class="title">Rechargez votre véhicule électrique à Angers <img src="assets/bolt.png" class="bolt" alt="Icone d'éclair"/></h1>
+    <h3 class="description">Trouvez à Angers différentes bornes de recharge pour véhicules électriques, gratuites ou payantes.</h3>
+    <main>
+        <iframe class="map" id="map" width="1000" height="500" src="carte.html"></iframe>
+        <div class="bottom">
+            <p class="text">Il y a <span id="amount"></span> bornes de recharge <span id="type"></span> dans la ville</p>
+            <div class="filter">
+                <p>Filtrer</p>
+                <div class="buttons">
+                    <button id="free">Gratuites</button>
+                    <button id="charged">Payantes</button>
+                </div>
+            </div>
 
+        </div>
+    </main>
+    <a class="logo" href="https://www.angers.fr/" target="_blank"><img class="logo" src="assets/angers.webp" alt="Logo de la ville d'Angers"/></a>
+
+    <img alt="Fond coloré" class="background" src="assets/gradient-bg.webp"/>
+
+    <script>
+    const freeButton = document.getElementById("free")
+const chargedButton = document.getElementById("charged")
+const map = document.getElementById("map")
+const amount = document.getElementById("amount")
+const type = document.getElementById("type")
+
+const totalStations = %s
+const freeStations = %s
+const chargedStations = %s
+
+amount.textContent = totalStations.toString()
+
+freeButton.addEventListener("click", () => {
+    chargedButton.classList.remove("active")
+    if(freeButton.classList.contains("active")){
+        freeButton.classList.remove("active")
+        map.src = "./carte.html"
+        amount.textContent = totalStations.toString()
+        type.textContent = ""
+    }else{
+        freeButton.classList.add("active")
+        map.src = "./carte_free.html"
+        amount.textContent = freeStations.toString()
+        type.textContent = "gratuites"
+    }
+})
+chargedButton.addEventListener("click", () => {
+    freeButton.classList.remove("active")
+    if(chargedButton.classList.contains("active")){
+        chargedButton.classList.remove("active")
+        map.src = "./carte.html"
+        amount.textContent = totalStations.toString()
+        type.textContent = ""
+    }else{
+        chargedButton.classList.add("active")
+        map.src = "./carte_charged.html"
+        amount.textContent = chargedStations.toString()
+        type.textContent = "payantes"
+    }
+})
+</script>
+</body>
+
+</html>
+    """ % (str(len(stations_payantes) + len(stations_gratuites)),
+           str(len(stations_gratuites)),
+           str(len(stations_payantes)))
+            )
